6315ddfc3e49491fd4f30c6c5adc26ac8cffa212
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-02-25 20:31:04 +0000

    Add basic js for Book constr and addBook func

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
